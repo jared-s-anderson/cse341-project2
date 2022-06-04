474bff7e69902bf7e1a4921aeed59c877bd743d4
jaredsanderson <jaredsa@byui.edu>
2022-06-03 21:06:22 -0600

    Environment Variables Updated

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const { auth, requiresAuth } = require('express-openid-connect');
-// require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// // Config vars need to be added in the heroku settings.
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.SECRET,
-//   baseURL: process.env.BASE_URL,
-//   clientID: process.env.CLIENT_ID,
-//   issuerBaseURL: process.env.ISSUER_BASE_URL
-// };
+// Config vars need to be added in the heroku settings.
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
 
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-// // req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
-// app.get('/profile', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.oidc.user));
-// });
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app
   .use(bodyParser.json())
