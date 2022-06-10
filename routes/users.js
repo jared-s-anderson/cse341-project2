eba8b76b1cf157db98e50f7b0676d83156bd4bfa
jaredsanderson <jaredsa@byui.edu>
2022-06-10 13:36:10 -0600

    Updated User Routes

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,12 @@ const validation = require('../middleware/validate');
 
 routes.get('/', userController.retrieveAll);
 
-routes.get(':/id', userController.retriveSingle);
+routes.get('/:id', userController.retriveSingle);
 
 routes.post('/', validation.saveUser, userController.addUser);
 
 routes.put('/:id', validation.saveUser, userController.updateUser);
 
-routes.delete(':/id', userController.deleteUser);
+routes.delete('/:id', userController.deleteUser);
 
 module.exports = routes;
